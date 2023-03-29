aa2276b4960e48234519dcc7a282c1b72d9529a3
swarajagrawal <swarajagrawal@yahoo.com>
2023-03-29 14:10:53 +0000

    worked till image upload to repository ECR

--- run_docker.sh
+++ run_docker.sh
@@ -2,11 +2,11 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
 # Build image and add a descriptive tag
+docker build --tag=project-ml-microservice-kubernetes .
 
-# Step 2: 
 # List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -p 8000:80 project-ml-microservice-kubernetes
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
+# This file tags and uploads an image to AWS Elastic Container Registry
 
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+# Retrieve an authentication token and authenticate your Docker client to your registry
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 647868873944.dkr.ecr.us-west-2.amazonaws.com
 
 # Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+# Tagging the image so that it can be pushed to the repository
+docker tag project-ml-microservice-kubernetes:latest 647868873944.dkr.ecr.us-west-2.amazonaws.com/project-ml-microservice-kubernetes:latest
 
 # Step 3:
-# Push image to a docker repository
+# Push this image to your newly created AWS repository
+docker push 647868873944.dkr.ecr.us-west-2.amazonaws.com/project-ml-microservice-kubernetes:latest
